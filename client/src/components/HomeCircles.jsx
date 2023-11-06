a7f45298bd4c11a3b32fc04a2646819f9417392b
Sumit Verma <sverma162003@gmail.com>
2023-11-06 12:25:45 +0530

    Changed the css

--- client/src/components/HomeCircles.jsx
+++ client/src/components/HomeCircles.jsx
@@ -4,67 +4,68 @@ import "../styles/homecircles.css";
 
 const HomeCircles = () => {
   return (
-    <section className="container circles">
-      <div className="circle">
-        <CountUp
-          start={0}
-          end={5000}
-          delay={0}
-          enableScrollSpy={true}
-          scrollSpyDelay={500}
-        >
-          {({ countUpRef }) => (
-            <div className="counter">
-              <span ref={countUpRef} />+
-            </div>
-          )}
-        </CountUp>
-        <span className="circle-name">
-          Registered
-          <br />
-          Patients
-        </span>
-      </div>
-      <div className="circle">
-        <CountUp
-          start={0}
-          end={750}
-          delay={0}
-          enableScrollSpy={true}
-          scrollSpyDelay={500}
-        >
-          {({ countUpRef }) => (
-            <div className="counter">
-              <span ref={countUpRef} />+
-            </div>
-          )}
-        </CountUp>
-        <span className="circle-name">
-          Certified
-          <br />
-          Doctors
-        </span>
-      </div>
-      <div className="circle">
-        <CountUp
-          start={0}
-          end={100}
-          delay={0}
-          enableScrollSpy={true}
-          scrollSpyDelay={500}
-        >
-          {({ countUpRef }) => (
-            <div className="counter">
-              <span ref={countUpRef} />+
-            </div>
-          )}
-        </CountUp>
-        <span className="circle-name">
-          Specialists
-          <br />
-        </span>
-      </div>
-    </section>
+    <></>
+    // <section className="container circles">
+    //   <div className="circle">
+    //     <CountUp
+    //       start={0}
+    //       end={5000}
+    //       delay={0}
+    //       enableScrollSpy={true}
+    //       scrollSpyDelay={500}
+    //     >
+    //       {({ countUpRef }) => (
+    //         <div className="counter">
+    //           <span ref={countUpRef} />+
+    //         </div>
+    //       )}
+    //     </CountUp>
+    //     <span className="circle-name">
+    //       Registered
+    //       <br />
+    //       Patients
+    //     </span>
+    //   </div>
+    //   <div className="circle">
+    //     <CountUp
+    //       start={0}
+    //       end={750}
+    //       delay={0}
+    //       enableScrollSpy={true}
+    //       scrollSpyDelay={500}
+    //     >
+    //       {({ countUpRef }) => (
+    //         <div className="counter">
+    //           <span ref={countUpRef} />+
+    //         </div>
+    //       )}
+    //     </CountUp>
+    //     <span className="circle-name">
+    //       Certified
+    //       <br />
+    //       Doctors
+    //     </span>
+    //   </div>
+    //   <div className="circle">
+    //     <CountUp
+    //       start={0}
+    //       end={100}
+    //       delay={0}
+    //       enableScrollSpy={true}
+    //       scrollSpyDelay={500}
+    //     >
+    //       {({ countUpRef }) => (
+    //         <div className="counter">
+    //           <span ref={countUpRef} />+
+    //         </div>
+    //       )}
+    //     </CountUp>
+    //     <span className="circle-name">
+    //       Specialists
+    //       <br />
+    //     </span>
+    //   </div>
+    // </section>
   );
 };
 
