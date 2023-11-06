51eb2d143b4260d81fe2154df40ce4a2fbe8e0d7
Mayur <mayurrokade1718@gmail.com>
2023-11-06 13:34:03 +0530

    footer added

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,77 +1,80 @@
-import React from "react";
-import "../styles/footer.css";
-import { FaFacebookF, FaYoutube, FaInstagram } from "react-icons/fa";
-import { HashLink } from "react-router-hash-link";
-import { NavLink } from "react-router-dom";
+import '../styles/footer.css'
+import { Link,NavLink } from "react-router-dom"
+
+
 
 const Footer = () => {
   return (
-    <>
-      <footer>
-        <div className="footer">
-          <div className="footer-links">
-            <h3>Links</h3>
-            <ul>
-              <li>
-                <NavLink to={"/"}>Home</NavLink>
-              </li>
-              <li>
-                <NavLink to={"/doctors"}>Doctors</NavLink>
-              </li>
-              <li>
-                <NavLink to={"/appointments"}>Appointments</NavLink>
-              </li>
-              <li>
-                <NavLink to={"/notifications"}>Notifications</NavLink>
-              </li>
-              <li>
-                <HashLink to={"/#contact"}>Contact Us</HashLink>
-              </li>
-              <li>
-                <NavLink to={"/profile"}>Profile</NavLink>
-              </li>
-            </ul>
-          </div>
-          <div className="social">
-            <h3>Social links</h3>
-            <ul>
-              <li className="facebook">
-                <a
-                  href="https://www.facebook.com/"
-                  target={"_blank"}
-                  rel="noreferrer"
-                >
-                  <FaFacebookF />
-                </a>
-              </li>
-              <li className="youtube">
-                <a
-                  href="https://www.youtube.com/"
-                  target={"_blank"}
-                  rel="noreferrer"
-                >
-                  <FaYoutube />
-                </a>
-              </li>
-              <li className="instagram">
-                <a
-                  href="https://www.instagram.com/"
-                  target={"_blank"}
-                  rel="noreferrer"
-                >
-                  <FaInstagram />
-                </a>
-              </li>
-            </ul>
-          </div>
+    <footer className="footer">
+    <div className="container footer__container">
+        <div className="footer__1">
+            <Link to="/" className="footer__logo"><h4>TELE MEDICINE</h4></Link>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam tenetur, accusantium id nostrum 
+                magni eaque!
+            </p>
         </div>
-        <div className="footer-bottom">
 
-         All Rights Reserved© {new Date().getFullYear()}
+        <div className="footer__2">
+            <h4>Permalinks</h4>
+            <ul className="permalinks">
+                <li><NavLink to={"/"}>Home</NavLink></li>
+                <li> <NavLink to={"/doctors"}>Doctors</NavLink></li>
+                <li><NavLink to={"/appointments"}>Appointments</NavLink></li>
+                <li><NavLink to={"/notifications"}>Notifications</NavLink></li>
+                <li><NavLink to={"/contact"}>Contact</NavLink></li>
+                <li> <NavLink to={"/profile"}>Profile</NavLink></li>
+            </ul>
         </div>
-      </footer>
-    </>
-  );
-};
 
-export default Footer;
+        <div className="footer__3">
+            <h4>Privacy</h4>
+        
+            <ul className="privacy">
+                <li><Link to="/">Primary Policy</Link></li>
+                <li><Link to="/">Terms and Conditions</Link></li>
+                <li><Link to="/">Refund Policy</Link></li>
+                
+            </ul>
+        </div>
+
+        <div className="footer__4">
+            <h4>Contact Us</h4>
+            <div>
+                <p>
+                    +91 9999999999
+                </p>
+                <p>
+                    abc@gmail.com
+                </p>
+            </div>
+
+            <ul className="footer__socials">
+                <li>
+                    <a href="a"><i className="fa-brands fa-facebook"></i></a>
+                </li>
+                <li>
+                    <a href="a"><i className="fa-brands fa-instagram"></i></a>
+                </li>
+                <li>
+                    <a href="a"><i className="fa-brands fa-twitter"></i></a>
+                </li>
+                <li>
+                    <a href="a"><i className="fa-brands fa-linkedin-in"></i></a>
+                </li>
+            </ul>
+        </div>
+
+       
+
+
+    </div>
+    <div className="footer__copyright">
+        <small>Copyright &copy | TELE MEDICINE</small>
+    </div>
+
+</footer>
+  )
+}
+
+export default Footer
